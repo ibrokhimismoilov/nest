55c3663a8cf94ee3f04ef1d41d07514c19c2fa9d
ibrokhim <mr017programmer@gmail.com>
2023-07-24 09:32:38 +0500

    models

---
+++ src/product/product.model.ts
@@ -0,0 +1,14 @@
+export class ProductModel {
+  image: string;
+  title: string;
+  price: string;
+  oldPrice: string;
+  credit: number;
+  calculatedRaiting: number;
+  description: string;
+  advantages: string;
+  disAdvantages: string;
+  categories: string[];
+  tags: string;
+  //   charat
+}
